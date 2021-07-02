7d1afd5bd5b36ce02167cd7384ab63f1ffdd24c5
lmh0823 <lmh0823@163.com>
2021-07-02 18:36:30 +0800

    修改了login.js

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+$(function() {
+    getUserInfo()
+})
+
+function getUserInfo() {
+    $.ajax({
+
+        method: 'GET',
+        url: '/my/userinfo',
+        headers: {
+            Authorization: localStorage.getItem('token') || ''
+        },
+
+        success: function(res) {
+            console.log(res);
+        }
+    })
+}
--- assets/js/login.js
+++ assets/js/login.js
@@ -59,8 +59,10 @@ $(function() {
             if (res.status !== 0) {
                 return layer.msg(res.message)
             }
+
             layer.msg('登陆成功')
-            localStorage.setItem('taken', res.taken)
+
+            localStorage.setItem('token', res.token)
             location.href = '/index.html'
         })
     })
